db7cc5a589b23eca2ed4004d12a696a5bc9ffc51
Bill2015 <jimmy3112015@gmail.com>
2021-06-26 14:02:25 +0800

    fix number bug

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 from typing         import List
-from PyQt5          import QtWidgets
-import socket       as SOCKET   ## socket連線模組 ##
-
 import json.encoder
 import threading    as THREAD   ## 多執行緒模組 ##
 import os           as OS       ## 調用操作系統命令，來達成建立文件，刪除文件，查詢文件的模組 ##
-import sys          as SYS
 import time         as TIME
 import pprint       as PPRINT
 import jieba_fast   as JIEBA
-
+import keyboard     as KEYBOARD
+import msvcrt       as MSVCRT
+import sys          as SYS
 ## 語音轉文字模組 ##
 import speech_recognition   as SPEECH_RECOGNIZE
-from Util.command           import ActionCommand, ActionParameter, Command
+from PyQt5                  import QtWidgets
+from Util.command           import Command
 from Util.condition         import Condition, NumberCondition, PinyionCondition, SimilarCondition, SimpleCondition, SynonymCondition, TranslateCondition
 from Util.model             import Model  
 from Interface.mainGame     import GameMainUi
-from google_trans_new       import google_translator  
 
 class CommandActivater():
     def __init__(self, command:Command, executeTime=1) -> None:
@@ -30,22 +28,24 @@ class CommandActivater():
     def setTimes( self, times ):
         self._executeTime = times
 
+
 class SpeechSensor():
     def __init__(self):
         pass
+        
 
     def speechToText(self):
 
         recorder = SPEECH_RECOGNIZE.Recognizer()
-        recorder.energy_threshold = 2200
+        recorder.energy_threshold = 500
 
         with SPEECH_RECOGNIZE.Microphone() as source:
             print('請說話：')
             #r.adjust_for_ambient_noise(source, duration=0.5)
-            recorder.dynamic_energy_threshold = False
+            recorder.dynamic_energy_threshold = True
 
             try:
-                audio = recorder.listen( source, timeout=1, phrase_time_limit=2)
+                audio = recorder.listen( source, timeout=2, phrase_time_limit=2)
                 #audio = r.listen(source)
                 text = recorder.recognize_google(audio, language='zh-tw')
                 flag = 0
@@ -85,21 +85,27 @@ class SpeechRecognizeAgent(THREAD.Thread):
         for keyWord in self._model.getKeyWordSet():
             JIEBA.add_word( keyWord, Model.KEY_WORD_WEIGHT )
 
-        self.doAction()
+
+       # self.doAction()
+
+
 
 
     def run(self):
-        while(  self._working == True and self._counter < 5 ):
-            print("Threading....{index}".format( index=self._counter ))
-            self._counter += 1
+        # 判斷是否在工作
+        while( self._working == True ):
+            # 偵測是否結束
+            if MSVCRT.kbhit() or KEYBOARD.is_pressed('q'):
+                print("程式結束")
+                return
             self.doAction()
             TIME.sleep(1)
 
+
     def doAction( self ):
-       
-
-
-        textSpeech = "公雞10次後使用回復藥15次"
+        print( "目前狀態 -------------------------> " , self._nowStatus )
+        # 取得語音指令
+        textSpeech = "攻擊6次"
         if( textSpeech == None ):
             return
         print( "語音輸入：" + textSpeech )
@@ -115,13 +121,13 @@ class SpeechRecognizeAgent(THREAD.Thread):
         pinyinToken:List[str]   = PinyionCondition.GeneratePinyinList( tokenTexts )
         englishToken:List[str]  = TranslateCondition.GenerateEnglishList( tokenTexts )
         # 初始化 Command
-        command                                 = None      # 指令
         countable                               = False     # 可量化 Flag
         commandList: List[CommandActivater]     = []        # 指令串列
         tempActivator                           = None
         # ========================================================================================================
         for tokenIndex, token in enumerate(tokenTexts):
-            executeTime                             = 1     # 量詞
+            command                                 = None      # 指令
+            executeTime                             = 1         # 量詞
             # 每種判斷取出
             for condition in self._conditions:
                 # 取出目前狀態可以判斷的指令
@@ -158,23 +164,28 @@ class SpeechRecognizeAgent(THREAD.Thread):
                 # 設定次數
                 if( executeTime > 1 and tempActivator != None ):
                     tempActivator.setTimes( executeTime )
+                    # tokenTexts.remove( token )              # 移除已經判斷過的字
                 # 有找到指令
                 elif( command != None ):
                     print( "搜尋結果： (" + condition.getConditionName() + ") 最相近的字串: ", command.getChineseName() )
-                    tempActivator       = CommandActivater( command )
-                    commandList.append( tempActivator )
-                    self._nowStatus     = command.nextStatus( self._nowStatus )
-                    countable           = command.countable()
-                    print( "目前狀態：" , self._nowStatus )
+                    tempActivator       = CommandActivater( command )           # 待執行指令暫存
+                    commandList.append( tempActivator )                         # 待執行的指令串列
+                    self._nowStatus     = command.nextStatus( self._nowStatus ) # 取出下個狀態
+                    countable           = command.countable()                   # 判斷此指令是否可數
+                     # tokenTexts.remove( token )                                  # 移除已經判斷過的字
                     break
         
         for cm in commandList:
-            print( "{command} 執行 {time} 次".format(command=cm.command().getChineseName(), time=cm.executeTime()) )
+            print( "指令：{command} 執行 {time} 次".format(command=cm.command().getChineseName(), time=cm.executeTime()) )
 
 
         # ========================================================================================================
-        # parameter                   = None
+        # countable                                   = False     # 可量化 Flag
+        # parameterList: List[CommandActivater]       = []        # 指令串列
+        # tempActivator                               = None
         # for tokenIndex, token in enumerate(tokenTexts):
+        #     parameter                               = None
+        #     executeTime                             = 1         # 量詞
         #     # 每種判斷取出
         #     for condition in self._conditions:
         #         # 取出目前狀態可以判斷的指令
@@ -183,8 +194,13 @@ class SpeechRecognizeAgent(THREAD.Thread):
         #             print( "未知狀態" )
         #             return
         #         # ------------------------------------------------------
+        #         # 數字判斷
+        #         if( isinstance(condition, NumberCondition) ):
+        #             if( countable == True ):
+        #                 executeTime = condition.execute( token )     
+        #         # ------------------------------------------------------
         #         # 拼音判斷
-        #         if( isinstance(condition, PinyionCondition) ):
+        #         elif( isinstance(condition, PinyionCondition) ):
         #             parameter = condition.execute( statusParameter, token,  pinyinToken[ tokenIndex ] )
         #             # 加入至相似詞裡
         #             if( parameter != None ):
@@ -202,14 +218,25 @@ class SpeechRecognizeAgent(THREAD.Thread):
         #         # 其他
         #         else:
         #             parameter = condition.execute( statusParameter,  token )
-        #         # ------------------------------------------------------   
+        #         # ------------------------------------------------------ 
+        #         # 設定次數
+        #         if( executeTime > 1 and tempActivator != None ):
+        #             tempActivator.setTimes( executeTime )
+        #             # tokenTexts.remove( token )              # 移除已經判斷過的字
         #         # 有找到指令
-        #         if( parameter != None ):
+        #         elif( parameter != None ):
         #             print( "搜尋結果： (" + condition.getConditionName() + ") 最相近的字串: ", parameter.getChineseName() )
-        #             self._nowStatus  = parameter.nextStatus( self._nowStatus )
+        #             tempActivator       = CommandActivater( parameter )           # 待執行指令暫存
+        #             parameterList.append( tempActivator )                         # 待執行的指令串列
+        #             self._nowStatus     = parameter.nextStatus( self._nowStatus ) # 取出下個狀態
+        #             countable           = parameter.countable()                   # 判斷此指令是否可數
+        #              # tokenTexts.remove( token )                                  # 移除已經判斷過的字
         #             print( "目前狀態：" , self._nowStatus )
         #             break
 
+
+        # for pm in parameterList:
+        #     print( "參數：{command} 執行 {time} 次".format(command=pm.command().getChineseName(), time=pm.executeTime()) )
         # self._model.saveDataToFile()
         
 
@@ -221,11 +248,11 @@ if __name__ == "__main__":
         # Initial
 
         agent       = SpeechRecognizeAgent()
-      #  agent.start()
-      #  app         = QtWidgets.QApplication( SYS.argv )
-      #  window      = GameMainUi()
-      #  window.show()
-      #  app.exec_()
+        agent.start()
+        app         = QtWidgets.QApplication( SYS.argv )
+        window      = GameMainUi()
+        window.show()
+        app.exec_()
 
     try:
         run_app()
